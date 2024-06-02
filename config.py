9d0a0c72689d3eafb7bc6c709b487f5db5dc14b6
1FireFly1 <151950177+1FireFly1@users.noreply.github.com>
2024-06-02 15:00:11 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+
+TELEGRAM_TOKEN = '6712872902:AAGa_3h6dn5pyXwPYJeidsEoBsy6D2X8vZw'
+FOLDER_ID = 'b1gi0idriqjv7c0jgpsk'
+IAM_TOKEN = 'y0_AgAAAAA_QYP8AATuwQAAAAEAQi3VAAAvAnwxDRJAqYW7chuMHbjzFDCNow'
+COUNT_LAST_MSG = 4
+MAX_USER_TTS_SYMBOLS = 800
+MAX_TTS_SYMBOLS = 200
+MAX_USER_STT_BLOCKS = 12
+MAX_USERS = 3
+MAX_GPT_TOKENS = 100
+MAX_USER_GPT_TOKENS = 10
+SYSTEM_PROMPT = [{'role': 'system',
+                  'text': 'Ты умный и дружелюбный собеседник. '
+                          'Общайся с пользователем на "ты" и используй юмор.'
+                          'Поддерживай диалог. Изображай человека'}]
+LOGS = 'logs.txt'
+PATH_TO_DB = 'messages.db'
